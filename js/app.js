af8c9e263adbc84f6c17a4991eb0d764d870d75a
Fredricksen <61914188+Fredricksen@users.noreply.github.com>
2020-06-25 12:57:58 -0500

    real2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 var OpenStreetMap_HOT = L.tileLayer(
   'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
   {
@@ -7,7 +6,6 @@ var OpenStreetMap_HOT = L.tileLayer(
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
   }
 );
-
 var OpenTopoMap = L.tileLayer(
   'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
   {
@@ -16,15 +14,15 @@ var OpenTopoMap = L.tileLayer(
       'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   }
 );
-var Standar = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+var Stadia_Outdoors = L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
+	maxZoom: 16,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 });
 
 let baseMaps = {
   'Open Street Map': OpenStreetMap_HOT,
   'Open Street Topo': OpenTopoMap,
-  'Open Standar' : Standar
+  'Open Stadia Outdoors' : Stadia_Outdoors
 };
 
 //let map = L.map('map').fitWorld();
@@ -33,19 +31,18 @@ let map = L.map('map',{
   layers: [
     OpenTopoMap
   ]
-}).fitWorld();
+}).fitWorld(); 
 
 L.control.layers(baseMaps, ).addTo(map); //CONTROL LAYRSMAPAS
 
-function eachLayer(layer) { // CADA CXADA UN MARCADOR
+function eachLayer(layer) { // CADA CAPA UN MARCADOR
   const layerData = layer.toGeoJSON(); // cargar datos a formato geojason
   //console.log(layerData)
   layer.options.title = layerData.properties.Nombre;
-  layer.bindPopup(`<h3>${layerData.properties.Nombre}</h3>`); // PARA CZADA MARCADOR UN POPUP
+  layer.bindPopup(`<h2>${layerData.properties.Nombre}</h2>`); // PARA CZADA MARCADOR UN POPUP
 }
 
 ///Users/Fredricksen/.bitnami/stackman/machines/xampp/volumes/root/htdocs/Cnveniodependencias/convleaflet/data/CPPCUVALLES.csv
-//                         Cnveniodependencias/convleaflet/data/ConveniosPracticasProfesionalesCUVALLES.csv
 let layer = omnivore.csv('/Cnveniodependencias/convleaflet/data/CPPCUVALLES.csv', {
 //let layer = omnivore.csv('/Cnveniodependencias/convleaflet/data/pruebat-rural.csv', {
   latfield: 'GPS.Latitud',
